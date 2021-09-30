16030dc4d6b3d538525cfca56b84f06bddad5b2a
Sean Redmon <seen360@yahoo.com>
2021-09-30 11:38:17 -0400

    NoFeedback page styled

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Switch, Route } from "react-router-dom";
 import HomePage from "./pages/HomePage";
+import NoFeedback from "./pages/NoFeedback";
 import NotFound from "./pages/NotFound";
 
 const Main = () => {
@@ -8,7 +9,8 @@ const Main = () => {
     <Switch>
       {/* The Switch decides which component to show based on the current URL.*/}
       <Route exact path="/not-found" component={NotFound}></Route>
-      <Route exact path="/" component={HomePage}></Route>
+      <Route exact path="/" component={NoFeedback}></Route>
+      {/* <Route exact path="/" component={HomePage}></Route> */}
     </Switch>
   );
 };
---
+++ src/components/pages/NoFeedback.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import AddFeedbackButton from "../AddFeedbackButton";
+import empty from "../../starter-code/assets/suggestions/empty.svg";
+
+const NoFeedback = () => {
+  return (
+    <div className="no-feedback">
+      <div className="__inner-container flex flex-ai-c flex-jc-c">
+        <div className="__not-found-image">
+          <img src={empty} alt="No feedback found" />
+        </div>
+        <span className="__header">There is no feedback yet.</span>
+        <p>
+          Got a suggestion? Found a bug that needs to be squashed? We love
+          hearing about new ideas to improve our app.
+        </p>
+        <AddFeedbackButton />
+      </div>
+    </div>
+  );
+};
+
+export default NoFeedback;
