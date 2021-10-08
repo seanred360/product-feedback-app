90e7f2846911a4c13371e4968656cb4a03c2b7be
Sean Redmon <seen360@yahoo.com>
2021-10-08 13:56:31 -0400

    Comment styling
    Added horizontal separation line between each comment except for the last comment.

---
+++ src/components/AddComment.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const AddComment = () => {
+    return ( <div className="add-comment"><h1>add comment</h1></div> );
+}
+ 
+export default AddComment;
--- src/components/CommentsSection.jsx
+++ src/components/CommentsSection.jsx
@@ -1,21 +1,29 @@
+import { react } from "@babel/types";
 import React from "react";
+import AddComment from "./AddComment";
 import Comment from "./Comment";
 
 const CommentsSection = ({ commentsCount, comments }) => {
   return (
-    <div className="comments-section">
-      <h1 className="comments-count">{commentsCount} Comments</h1>
-      {comments && comments.map((comment) => (
-        <Comment
-          key={comment["content"]}
-          userImage={comment["user"]["image"]}
-          userName={comment["user"]["name"]}
-          userAlias={comment["user"]["username"]}
-          content={comment["content"]}
-          replies={comment['replies']}
-        />
-      ))}
-    </div>
+    <React.Fragment>
+      <div className="comments-section">
+        <div className="comments">
+          <h1 className="comments-count">{commentsCount} Comments</h1>
+          {comments &&
+            comments.map((comment) => (
+              <Comment
+                key={comment["content"]}
+                userImage={comment["user"]["image"]}
+                userName={comment["user"]["name"]}
+                userAlias={comment["user"]["username"]}
+                content={comment["content"]}
+                replies={comment["replies"]}
+              />
+            ))}
+        </div>
+      </div>
+      <AddComment />
+    </React.Fragment>
   );
 };
 
