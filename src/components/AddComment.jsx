be768fcdd7de8a0a9061ea3f2de3469fb93c0cff
Sean Redmon <seen360@yahoo.com>
2021-10-08 15:09:12 -0400

    Add comment styled

--- src/components/AddComment.jsx
+++ src/components/AddComment.jsx
@@ -1,7 +1,18 @@
-import React from 'react';
+import React from "react";
 
 const AddComment = () => {
-    return ( <div className="add-comment"><h1>add comment</h1></div> );
-}
- 
+  return (
+    <div className="add-comment">
+      <h2 className="__header">Add Comment</h2>{" "}
+      <form action="">
+        <input type="text" placeholder="Type your comment here" />
+      </form>
+      <div className="__bottom flex flex-ai-c flex-jc-sb">
+        <span className="--characters-remaining">250 Characters left</span>
+        <button className="all-buttons --purple-button">Post Comment</button>
+      </div>
+    </div>
+  );
+};
+
 export default AddComment;
--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -1,28 +1,3 @@
-// import React from "react";
-
-// const Comment = ({ userImage, userName, userAlias, content, replies }) => {
-//   return (
-//     <div className="comment">
-//       <div className="__user flex">
-//         <img
-//           className="--user-image"
-//           src={process.env.PUBLIC_URL + userImage}
-//           alt="user"
-//           className="--user-image"
-//         />
-//         <div className="--user-text">
-//           <span className="--user-name">{userName}</span>
-//           <span className="--user-alias">{userAlias}</span>
-//         </div>
-//         <button className="--reply-button all-buttons">Reply</button>
-//       </div>
-//       <p className="__content">{content}</p>
-//     </div>
-//   );
-// };
-
-// export default Comment;
-
 import React, { Component } from "react";
 import ReplyComment from "./ReplyComment";
 
