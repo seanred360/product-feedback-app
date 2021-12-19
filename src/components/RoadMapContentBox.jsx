dbb77341dff97af78852415a3c52eeca672fad87
Sean Redmon <seen360@yahoo.com>
2021-12-19 17:47:33 -0500

    Update RoadMapContentBox.jsx
    Clicking RoadMapContentBox redirects to the feedback page for the selected product

--- src/components/RoadMapContentBox.jsx
+++ src/components/RoadMapContentBox.jsx
@@ -1,18 +1,27 @@
-import React from "react";
+import React, { useContext } from "react";
 import CategoryButton from "./common/CategoryButton";
 import UpVote from "./common/UpVote";
 import CommentsCounter from "./common/CommentsCounter";
-import _ from 'lodash';
+import _ from "lodash";
+import { ProductRequestContext } from "../custom-hooks/Contexts";
+import { useHistory } from "react-router";
 
-const RoadMapContentBox = ({content}) => {
+const RoadMapContentBox = ({ content }) => {
+  const { setSelectedProductRequest } = useContext(ProductRequestContext);
+  const history = useHistory();
+
+  const handleClick = (product) => {
+    console.log("click");
+    setSelectedProductRequest(product);
+    history.push("/feedback-detail");
+  };
+
   return (
-    <div className="__roadmap-content-box">
+    <div className="__roadmap-content-box" onClick={() => handleClick(content)}>
       <li className="__content-status">{content["status"]}</li>
 
       <span className="__content-name">{content["title"]}</span>
-      <p className="__content-description">
-        {content["description"]}
-      </p>
+      <p className="__content-description">{content["description"]}</p>
       <CategoryButton itemName={"Feature"} />
       <div className="__bottom">
         <UpVote upvotes={content["upvotes"]} />
