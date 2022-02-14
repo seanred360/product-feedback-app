ee689d04bd4a130ebcec0959f039f116bdc1e698
Sean Redmon <seen360@yahoo.com>
2022-02-14 16:22:48 -0500

    SignupPage implemented

--- src/components/pages/SignupPage.jsx
+++ src/components/pages/SignupPage.jsx
@@ -2,84 +2,126 @@ import TextInput from "../common/TextInput";
 import { useAuth } from "../../custom-hooks/AuthContext";
 import { useState } from "react/cjs/react.development";
 import { Link, useHistory } from "react-router-dom";
+import Joi from "joi-browser";
 
 const Signup = () => {
   const { signup } = useAuth();
-  const [error, setError] = useState("");
+  const [error, setError] = useState({});
   const [loading, setLoading] = useState(false);
   const [formData, setFormData] = useState({
-    displayName: "",
+    name: "",
     email: "",
     password: "",
     passwordConfirm: "",
   });
   const history = useHistory();
 
+  const schema = {
+    name: Joi.string().required().label("Display Name"),
+    email: Joi.string().email().required().label("Email"),
+    password: Joi.string().required().label("Password"),
+    passwordConfirm: Joi.string().required().label("Password Confirm"),
+  };
+
+  const validate = () => {
+    const options = { abortEarly: false };
+    const { error } = Joi.validate(formData, schema, options);
+    if (!error) return null;
+
+    const errors = {};
+    for (let item of error.details) errors[item.path[0]] = item.message;
+    console.log(errors);
+    return errors;
+  };
+
+  const validateProperty = ({ name, value }) => {
+    const obj = { [name]: value };
+    const propertySchema = { [name]: schema[name] };
+    const { errors } = Joi.validate(obj, propertySchema);
+    return errors ? errors.details[0].message : null;
+  };
+
   const handleChange = (input) => {
+    const errors = { ...error };
+    const errorMessage = validateProperty(input);
+    if (errorMessage) errors[input.name] = errorMessage;
+    else delete errors[input.name];
+
     let data = { ...formData };
     data[input.name] = input.value;
     setFormData(data);
+    setError(errors);
   };
 
   const handleSubmit = async (e) => {
     e.preventDefault();
 
+    const errors = validate();
+    setError(errors || {});
+    if (errors) return;
+
     if (formData.password !== formData.passwordConfirm)
-      return setError("Passwords do not match");
+      return setError({ ...errors, passwordConfirm: "Passwords do not match" });
 
     try {
-      setError("");
       setLoading(true);
-      await signup(formData.displayName, formData.email, formData.password);
+      await signup(formData.name, formData.email, formData.password);
       history.push("/");
-    } catch {
-      setError("Failed to create an account");
+    } catch (err) {
+      console.log(err);
+      setLoading(false);
     }
-    setLoading(false);
   };
 
   return (
-    <form onSubmit={handleSubmit}>
-      <TextInput
-        name={"displayName"}
-        label={"Display Name"}
-        type={"text"}
-        instructions={"Other users will see this in your comments and posts"}
-        autoFocus={true}
-        onChange={(e) => handleChange(e.target)}
-        error={error}
-      />
-      <TextInput
-        name={"email"}
-        label={"email"}
-        type={"email"}
-        instructions={""}
-        onChange={(e) => handleChange(e.target)}
-        error={error}
-      />
-      <TextInput
-        name={"password"}
-        label={"password"}
-        type={"password"}
-        instructions={""}
-        onChange={(e) => handleChange(e.target)}
-        error={error}
-      />
-      <TextInput
-        name={"passwordConfirm"}
-        label={"Password Confirmation"}
-        type={"password"}
-        instructions={""}
-        onChange={(e) => handleChange(e.target)}
-        error={error}
-      />
-      <button type="submit" disabled={loading}>
-        Sign Up
-      </button>
-      <div>
-        Already have an account? <Link to="/log-in">Log In</Link>
-      </div>
-    </form>
+    <div className="signup-page">
+      <form onSubmit={handleSubmit}>
+        <h1>Sign Up</h1>
+        <TextInput
+          name={"name"}
+          label={"display name"}
+          type={"text"}
+          instructions={"Other users will see this in your comments and posts"}
+          autoFocus={true}
+          onChange={(e) => handleChange(e.target)}
+          error={error.hasOwnProperty("name") && error["name"]}
+        />
+        <TextInput
+          name={"email"}
+          label={"email"}
+          type={"email"}
+          instructions={"Your email is hidden from other users"}
+          onChange={(e) => handleChange(e.target)}
+          error={error.hasOwnProperty("email") && error["email"]}
+        />
+        <TextInput
+          name={"password"}
+          label={"password"}
+          type={"password"}
+          onChange={(e) => handleChange(e.target)}
+          error={error.hasOwnProperty("password") && error["password"]}
+        />
+        <TextInput
+          name={"passwordConfirm"}
+          label={"password confirmation"}
+          type={"password"}
+          onChange={(e) => handleChange(e.target)}
+          error={
+            error.hasOwnProperty("passwordConfirm") && error["passwordConfirm"]
+          }
+        />
+        <button
+          className="all-buttons --gradiant-button"
+          type="submit"
+          disabled={validate()}
+        >
+          Sign Up
+        </button>
+        <div>
+          Already have an account? <Link to="/log-in">Log In</Link>
+        </div>
+      </form>
+    </div>
   );
 };
 
